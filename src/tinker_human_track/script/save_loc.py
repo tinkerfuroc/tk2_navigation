94e9151391d5e38852ac731a499d85eee6f19abd
gjc13 <amtcmp@126.com>
2016-04-12 19:12:31 +0800

    [Change] Save laser data

--- src/tinker_human_track/script/save_loc.py
+++ src/tinker_human_track/script/save_loc.py
@@ -6,16 +6,23 @@ import yaml
 from sensor_msgs.msg import LaserScan
 from numpy import array, save, arange
 from math import sin, cos
+import numpy
 
+def is_valid(loc):
+    if -1 < loc[0]  < 1 and -1 < loc[1] < 1:
+        return 1
+    return 0
 
 frame_id = 0
 def save_loc(laser_scan):
     global frame_id
     angles = arange(laser_scan.angle_min, laser_scan.angle_max - laser_scan.angle_increment, laser_scan.angle_increment)
     assert len(angles) == len(laser_scan.ranges)
-    loc = [[radius * cos(angle), radius * sin(angle)] for radius, angle in zip(laser_scan.ranges, angles)]
-    if frame_id % 100 == 0:
-        save('frame%d' % (frame_id/100), array(loc))
+    ranges = [min(laser_scan.range_max, r) for r in ranges]
+    locs = [[radius * cos(angle), radius * sin(angle)] for radius, angle in zip(ranges, angles)]
+    result = [is_valid(loc) for loc in locs]
+    save('frame%d' % frame_id, array(ranges, dtype='f'))
+    save('result%d' % frame_id, array(result, dtpe=numpy.uint8))
     frame_id += 1
 
 
