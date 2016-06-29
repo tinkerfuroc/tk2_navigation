8cebf0e48ed11c483cf23efffe438fa511e9ea77
gjc13 <amtcmp@126.com>
2016-06-29 12:43:58 +0200

    [Add] Follow action

--- tinker_human_track/script/simple_follow.py
+++ tinker_human_track/script/simple_follow.py
@@ -3,61 +3,78 @@
 import sys
 import rospy
 from people_msgs.msg import People
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PointStamped
+from tinker_msgs.msg import FollowGoal
+from tinker_msgs.msg import FollowAction
+from tinker_msgs.msg import FollowResult
+from actionlib_msgs.msg import GoalStatus
 import tf
 from math import acos, sqrt, cos, sin
 from threading import Lock
 
+import actionlib
 
-pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)
-seq = 0
-l = Lock()
-now_target = None
-pose = None
-def found_people_handler(people):
-    global seq, pose, l
-    if len(people.people) > 0:
-        with l:
-            person = people.people[0]
-            x = person.position.x
-            y = person.position.y
-            rospy.loginfo('x at %f, y at %f' % (x, y))
-            if x*x + y*y < 4:
-                rospy.loginfo('found human, too close')
-                return
-            pose = PoseStamped()
-            pose.header.seq = seq
-            seq += 1
-            pose.header.stamp = rospy.Time.now()
-            pose.header.frame_id = people.header.frame_id
+class Follower:
+    def __init__(self):
+        self.goal_pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)
+        self.follow_server = rospy.actionlib.SimpleActionServer('follow_action',
+                            FollowAction,
+                            auto_start = False)
+        self.follow_server.register_goal_callback(self.goal_cb)
+        self.follow_server.register_preempt_callback(self.preempt_cb)
+        self.human_sub = rospy.Subscriber('tinker_vision/found_people', PointStamped, 
+                self.found_people_handler)
+        self.is_following = False
+        self.last_loc = PointStamped()
+        self.follow_server.start()
 
-            pose.pose.position.z = 0
-            distance = sqrt(x*x + y*y)
-            theta = acos(x / distance)
-            if x < 0:
-                theta = -theta
-            move_distance = distance - 2
-            pose.pose.position.x = move_distance * cos(theta)
-            pose.pose.position.y = move_distance * sin(theta)
-            quat= tf.transformations.quaternion_from_euler(0, 0, theta)
-            pose.pose.orientation.x = quat[0]
-            pose.pose.orientation.y = quat[1]
-            pose.pose.orientation.z = quat[2]
-            pose.pose.orientation.w = quat[3]
+    def found_people_handler(self, loc):
+        if not self.is_following:
+            return
+        x = loc.point.x
+        y = loc.point.y
+        if x == 0 and y == 0:
+            result = FollowResult()
+            result.last_found_loc = last_loc
+            self.follow_server.set_succeeded(result)
+            self.is_following = False
+        self.last_loc = loc
+        rospy.logdebug('x at %f, y at %f' % (x, y))
+        if x*x + y*y < 2:
+            rospy.logdebug('found human, too close')
+            return
+        pose = PoseStamped()
+        pose.header.seq = seq
+        seq += 1
+        pose.header = loc.header
+        pose.pose.position.z = 0
+        distance = sqrt(x*x + y*y)
+        theta = acos(x / distance)
+        if x < 0:
+            theta = -theta
+        move_distance = distance - 2
+        pose.pose.position.x = move_distance * cos(theta)
+        pose.pose.position.y = move_distance * sin(theta)
+        quat= tf.transformations.quaternion_from_euler(0, 0, theta)
+        pose.pose.orientation.x = quat[0]
+        pose.pose.orientation.y = quat[1]
+        pose.pose.orientation.z = quat[2]
+        pose.pose.orientation.w = quat[3]
+        self.goal_pub.publish(pose)
+
+    def goal_cb(self):
+        self.follow_server.acceptNewGoal()
+        self.is_following = True
+
+    def preempt_cb(self):
+        self.follow_server.set_preempted()
 
 
 def init(argv):
     global pub, pose, l
     rospy.init_node('simple_follow')
-    rospy.Subscriber('tinker_laser_human_tracker/laser_found_people', People, found_people_handler)
-    rate = rospy.Rate(1)
-    while not rospy.is_shutdown():
-        with l:
-            if pose != None:
-                rospy.loginfo('move to target x %f y %f' % (pose.pose.position.x, pose.pose.position.y))
-                pub.publish(pose)
-                pose = None
-        rate.sleep()
+    follow_action = Follower()
+    rospy.spin()
 
 
 if __name__ == '__main__':
